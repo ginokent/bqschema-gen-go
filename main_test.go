3ee1a004cffab8cf4342aaac807a3f5a299a0d63
djeeno <29125616+djeeno@users.noreply.github.com>
2020-11-26 22:33:10 +0900

    wip

--- main_test.go
+++ main_test.go
@@ -15,7 +15,8 @@ func Test_bigqueryFieldTypeToGoType(t *testing.T) {
 		bigquery.FloatFieldType:     reflect.Float64.String(),
 		bigquery.BooleanFieldType:   reflect.Bool.String(),
 		bigquery.TimestampFieldType: typeOfGoTime.String(),
-		// TODO(djeeno): bigquery.RecordFieldType: "",
+		// TODO(djeeno): support bigquery.RecordFieldType
+		//bigquery.RecordFieldType: "",
 		bigquery.DateFieldType:      typeOfDate.String(),
 		bigquery.TimeFieldType:      typeOfTime.String(),
 		bigquery.DateTimeFieldType:  typeOfDateTime.String(),
