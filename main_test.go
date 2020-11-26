f68a3b848a6b2073519d258c5a3246a83af908e7
djeeno <29125616+djeeno@users.noreply.github.com>
2020-11-26 21:39:14 +0900

    wip

--- main_test.go
+++ main_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+)
+
+func Test_bigqueryFieldTypeToGoType(t *testing.T) {
+	supportedBigqueryFieldTypes := map[bigquery.FieldType]inter{}{
+		bigquery.StringFieldType,
+		string(bigquery.BytesFieldType),
+		string(bigquery.IntegerFieldType),
+		string(bigquery.FloatFieldType),
+		string(bigquery.BooleanFieldType),
+		string(bigquery.TimestampFieldType),
+		//string(bigquery.RecordFieldType),
+		string(bigquery.DateFieldType),
+		string(bigquery.TimeFieldType),
+		string(bigquery.DateTimeFieldType),
+		string(bigquery.NumericFieldType),
+		string(bigquery.GeographyFieldType),
+	}
+
+	unsupportedBigqueryFieldTypes := []string{
+		string(bigquery.RecordFieldType),
+	}
+
+	for _, fieldType := range supportedBigqueryFieldTypes {
+		bigqueryFieldTypeToGoType(fieldType)
+	}
+
+	for _, fieldType := range unsupportedBigqueryFieldTypes {
+
+	}
+}
